bcfc947c1932792ae95086395ee41dec5a3e13f1
Gary Reeves <pkmnman@outlook.com>
2022-04-14 01:50:31 -0400

    Removed previous event chack

--- master_sniffer/views.py
+++ master_sniffer/views.py
@@ -26,31 +26,36 @@ def list_events(request):
             #sniffer_query = Device.objects.filter(serial_num = event.sniffer_serial, active=True)
 
             # Make sure we don't receive repeat request
+            '''
             prev_event = TrackingEvent.objects\
                 .filter(beacon_addr=serializer.validated_data['beacon_addr'])\
                 .order_by('event_time')[0]
-
+            '''
+            event = serializer.create(serializer.validated_data)
+            logging.info('Tracking event from %s created! Beacon MAC %s detected.', event.sniffer_serial,
+                         event.beacon_addr)
+            event.save()
+            req = requests.request(
+                method='POST',
+                url=WEB_SERVER_URL,
+                json={
+                    'sniffer_serial': event.sniffer_serial,
+                    'beacon_addr': event.beacon_addr,
+                    'event_time': event.event_time,
+                    'rssi': event.rssi
+                }
+            )
+            logging.info('Update request added to queue.')
+            # Then queue this event to get pushed to the webserver
+            # REQUEST_QUEUE.put_nowait(req)
+            return JsonResponse(serializer.data, content_type='application/json', status=status.HTTP_201_CREATED)
+            '''
             if all([prev_event.sniffer_serial == serializer.validated_data['sniffer_serial'],
                     (serializer.validated_data['event_time'] - prev_event.event_time) < timedelta(minutes=5)]):
-                event = serializer.create(serializer.validated_data)
-                logging.info('Tracking event from %s created! Beacon MAC %s detected.', event.sniffer_serial, event.beacon_addr)
-                event.save()
-                req = requests.request(
-                    method='POST',
-                    url=WEB_SERVER_URL,
-                    json= {
-                        'sniffer_serial': event.sniffer_serial,
-                        'beacon_addr': event.beacon_addr,
-                        'event_time': event.event_time,
-                        'rssi': event.rssi
-                    }
-                )
-                logging.info('Update request added to queue.')
-                # Then queue this event to get pushed to the webserver
-                #REQUEST_QUEUE.put_nowait(req)
-                return JsonResponse(serializer.data, content_type='application/json', status=status.HTTP_201_CREATED)
+                
             else:
                 logging.info('Duplicate event detected, ignoring!')
                 return JsonResponse(serializer.data, status=status.HTTP_208_ALREADY_REPORTED)
+            '''
 
         return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
