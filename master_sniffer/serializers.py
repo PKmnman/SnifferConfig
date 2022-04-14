9608ed23bbea5472bd85f5242987b5420302ac6d
Gary Reeves <pkmnman@outlook.com>
2022-04-13 23:50:28 -0400

    Fixed time format error

--- master_sniffer/serializers.py
+++ master_sniffer/serializers.py
@@ -5,10 +5,7 @@ class TrackingEventSerializer(serializers.Serializer):
 
     sniffer_serial = serializers.CharField(required=True)
     beacon_addr = serializers.CharField(required=True, allow_blank=False)
-    event_time = serializers.DateTimeField(
-        format="%m-%d-%y %H:%M:%S.%fz",
-        input_formats="%m-%d-%y %H:%M:%S.%fz",
-        required=True)
+    event_time = serializers.DateTimeField(required=True)
     rssi = serializers.IntegerField(required=True)
 
     def update(self, instance, validated_data):
