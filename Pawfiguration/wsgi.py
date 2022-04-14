451a3d7c94f180aed5cd467e25190f552f40886d
Gary Reeves <pkmnman@outlook.com>
2022-04-14 01:36:14 -0400

    Changed thread management

--- Pawfiguration/wsgi.py
+++ Pawfiguration/wsgi.py
@@ -9,6 +9,11 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 
 import os
 from django.core.wsgi import get_wsgi_application
+import signal
+
+import Pawfiguration
+
+signal.signal(signal.SIGTERM, Pawfiguration.request_handler.stop)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Pawfiguration.settings')
 
